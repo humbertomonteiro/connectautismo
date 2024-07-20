12cfa6e5fa2b7eaad125a40b54b2a18c00f444b8
Humberto Monteiro <humbertoaraujox@icloud.com>
2024-07-20 11:20:17 -0300

    att

--- src/components/BoxSlider/index.jsx
+++ src/components/BoxSlider/index.jsx
@@ -1,24 +1,31 @@
-import styles from "./boxSlider.module.css";
+import "./boxSlider.css";
 
 export default function BoxSlider({ array }) {
   return (
-    <div className={styles.boxSlide}>
+    <div className="boxSlide">
       {array.length > 1 ? (
         <div>
-          {array.map((item) => (
-            <div className={styles.showSlides}>
-              {array?.map((e) => (
-                <div key={e.name} className={styles.slide}>
-                  <img src={e.img} alt={e?.name} />
-                </div>
-              ))}
-            </div>
-          ))}
+          <div className="showSlides">
+            {array?.map((e) => (
+              <div
+                key={e.name}
+                className={
+                  array.length === 4
+                    ? "slide"
+                    : array.length === 3
+                    ? "slide3"
+                    : "slide2"
+                }
+              >
+                <img src={e.img} alt={e?.name} />
+              </div>
+            ))}
+          </div>
         </div>
       ) : (
         <>
           {array?.map((item) => (
-            <div className={styles.slideShow}>
+            <div className="slideShow">
               <img key={item.name} src={item.img} alt={item?.name} />
             </div>
           ))}
